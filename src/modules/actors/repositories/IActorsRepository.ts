d25622441f5db9209fdb987fb5aaec56c2bfd305
Jhonatec <jhonsreis@gmail.com>
2023-07-17 23:46:50 -0300

    Get with query + delete + post + find by id

--- src/modules/actors/repositories/IActorsRepository.ts
+++ src/modules/actors/repositories/IActorsRepository.ts
@@ -2,9 +2,10 @@ import { Actor } from "../models/Actor";
 
 interface IActorRepository {
   create(actor: Actor): Promise<Actor>;
-  findByName(name: string): Promise<Actor>;
+  // findByName(name: string): Promise<Actor[]>;
   findById(id: string | number): Promise<Actor>;
-  findAll(): Promise<Actor[]>;
+  findAll(name, order, limit): Promise<Actor[]>;
+  delete(id : string | number): Promise<Actor>;
 }
 
 export { IActorRepository };
--- src/modules/actors/useCases/listActors/ListActorsUseCase.ts
+++ src/modules/actors/useCases/listActors/ListActorsUseCase.ts
@@ -1,10 +1,11 @@
 import { Actor } from "../../models/Actor";
 import { IActorRepository } from "../../repositories/IActorsRepository";
 
-class ListActorsUseCase{
-  constructor(private actorRepository: IActorRepository){}
-  async execute(): Promise<Actor[]> {
-    return this.actorRepository.findAll();
+class ListActorsUseCase {
+  constructor(private actorRepository: IActorRepository) {}
+  async execute(name: string, order: string, limit: string): Promise<Actor[]> {
+
+    return this.actorRepository.findAll(name, order, limit);
   }
 }
 
